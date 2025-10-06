e9c20620fa260aed60177d56a697f260d66dec31
Carlaa88 <mdelpcar21@gmail.com>
2025-10-06 23:26:23 +0100

    Gestor Estudiante

--- AUT02_01/src/main/java/org/example/GestorEstudiante.java
+++ AUT02_01/src/main/java/org/example/GestorEstudiante.java
@@ -1,6 +1,36 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GestorEstudiante {
 
+    private ArrayList<Estudiante>estudiantes;
+    double suma = 0;
 
+    public GestorEstudiante(){
+        this.estudiantes = new ArrayList<>();
+    }
+
+    public void crearEstudiante(Estudiante nuevoEstudiante) {
+       if(nuevoEstudiante==null){return;}
+       estudiantes.add(nuevoEstudiante);
+    }
+
+//    private static void listarEstudiante() {
+//    }
+
+    public Estudiante buscarEstudiante(String nombre) {
+
+        for(int i=0; i<estudiantes.size();i++){
+            if(estudiantes.get(i).getNombre().equalsIgnoreCase(nombre)){
+              return estudiantes.get(i); //si se encuentra
+            }
+        }
+       return null; //si no se encuentra
+    }
+
+//    public double calcularMedia() {}
+//    private static void mejorNota() {
+//    }
 }
--- AUT02_01/src/main/java/org/example/Main.java
+++ AUT02_01/src/main/java/org/example/Main.java
@@ -6,46 +6,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-            mostrarMenu();
+        GestorEstudiante gestor = new GestorEstudiante();
+        gestionarMenu(gestor);
     }
 
-    //controlar : nombre vacios, nombre de solo letras, edades o notas negativas, opciones de menú fuera de rango,
-    //nombres repetidos ? cómo los buscamos?
-    //se buscan subcadenas del nombre ?
-
-
-    public static void mostrarMenu(){
+    public static void gestionarMenu(GestorEstudiante gestor){
         Scanner listaOpciones = new Scanner(System.in); //Recojo las respuestas
-        boolean salir = true;
-        int opcion;
+        boolean salir = false;
+        String select;
+        int opcion = 0;
 
         do{
-            System.out.println("===Gestor de Estudiantes===");
-            System.out.println("1. Añadir estudiante");
-            System.out.println("2. Listar estudiante");
-            System.out.println("3. Buscar estudiante por nombre");
-            System.out.println("4. Calcular nota media general");
-            System.out.println("5. Mostrar estudiante con mejor nota");
-            System.out.println("6. Salir");
-
-            System.out.print("Elige una opción:\n");
-            opcion = listaOpciones.nextInt();
-//            if(opcion>=1 && opcion<=6)
-//            {
-//
-//            }
-
-            listaOpciones.nextLine(); //limpiar buffer
+            System.out.println();
+            menu();
+            do{
+                System.out.print("Elige una opción (1-6):\n");
+                try{
+                    select = listaOpciones.nextLine();
+                    opcion = Integer.parseInt(select);
+                    if(opcion<1||opcion>6){
+                        System.out.println("Opción no válida. Debe ser entre 1 y 6. Prueba de nuevo");
+                    }
+                }catch (Exception e){
+                    System.out.println("Debes introducir un número entero");
+                    opcion = 0; //aseguro que siga el bucle
+                }
+            }while(opcion<1||opcion>6);
 
             switch (opcion) {
                 case 1:
-//                    crearEstudiante();
+                    añadirEstudiante(gestor, listaOpciones);
                     break;
                 case 2:
 //                    listarEstudiante();
                     break;
                 case 3:
-//                    buscarEstudiante();
+                     buscarEstudiante(gestor, listaOpciones);
                     break;
                 case 4:
 //                    calcularMedia();
@@ -54,12 +50,107 @@ public class Main {
 //                    mejorNota();
                     break;
                 case 6:
-                    salir = false; //arreglar
+                    salir = true; //arreglar
                     System.out.print("Has salido del programa");
             }
-        }while(salir);
+        }while(!salir);
+
 
     }
+
+    public static void menu(){
+        System.out.println("===Gestor de Estudiantes===");
+        System.out.println("1. Añadir estudiante");
+        System.out.println("2. Listar estudiante");
+        System.out.println("3. Buscar estudiante por nombre");
+        System.out.println("4. Calcular nota media general");
+        System.out.println("5. Mostrar estudiante con mejor nota");
+        System.out.println("6. Salir");
+    }
+
+    public static void añadirEstudiante(GestorEstudiante gestor, Scanner sc){
+        String nombre;
+        int edad;
+        double media;
+
+        //Validar nombre
+        do{
+            System.out.print("Nombre: ");
+            nombre = sc.nextLine().trim();
+            if(!nombreValido(nombre)){
+                System.out.println("El nombre no puede estar vacío. Solo puede contener letras y espacios.");
+            }
+
+            //Comprobar si ya existe el nombre
+            if(gestor.buscarEstudiante(nombre)!=null){
+                System.out.println("Ya existe un estudiante con ese nombre. No se añadirá otro igual.\n");
+                nombre = "";
+            }
+
+        }while(!nombreValido(nombre));
+
+       //Validar edad
+        do {
+            System.out.print("Edad: ");
+            while (!sc.hasNextInt()) {
+                System.out.println("Debes introducir un número entero para la edad.");
+                sc.next(); // descarta entrada incorrecta
+            }
+            edad = sc.nextInt();
+            if (edad <= 0) {
+                System.out.println("La edad debe ser mayor que cero.");
+            }
+            sc.nextLine(); // limpiar salto de línea pendiente
+        }while(edad<=0);
+
+        //Validar media
+        do{
+            System.out.print("Media: ");
+            while (!sc.hasNextDouble()) {
+                System.out.println("Debes introducir un número decimal para la media.");
+                sc.next();
+            }
+            media = sc.nextDouble();
+            if (media < 0 || media > 10) {
+                System.out.println("La media debe estar entre 0 y 10.");
+            }
+            sc.nextLine(); // limpiar salto de línea pendiente
+        }while(media < 0 || media > 10);
+
+        //Crear un estudiante
+        Estudiante nuevo = new Estudiante(nombre, edad, media, true);
+        gestor.crearEstudiante(nuevo);
+
+        System.out.println("Estudiante añadido correctamente.\n");
+
+    }
+
+    public static void buscarEstudiante(GestorEstudiante gestor, Scanner sc) {
+        System.out.print("Introduce el nombre a buscar: ");
+        String nombre = sc.nextLine();
+        Estudiante estudiante = gestor.buscarEstudiante(nombre);
+        if (estudiante != null) {
+            System.out.println("Estudiante encontrado: " + estudiante + "\n");
+        } else {
+            System.out.println("No se ha encontrado ningún estudiante.\n");
+        }
+    }
+
+    //VALIDACIONES
+    public static boolean nombreValido(String nombre)
+    {
+        char c;
+        if(nombre.isEmpty()) return false;
+        for(int i=0; i<nombre.length(); i++) {
+            c = nombre.charAt(i);
+            if(!Character.isLetter(c) && c != ' '){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 }
 
